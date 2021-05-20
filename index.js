b6a1c6072df9bee3230a11f4ff3455aebd0cdfd2
ASHRAFUL <alammdashrafulcst@gmail.com>
2021-05-21 00:03:15 +0600

    Initial four new file added

---
+++ index.js
@@ -0,0 +1,8 @@
+
+
+document.write ("i like my girl  <br/>  ");
+
+
+document.write("I am ashraful   <br/> ");
+
+document.write("I am from Bangladesh. living in Bangladesh . I like doing programming.");
